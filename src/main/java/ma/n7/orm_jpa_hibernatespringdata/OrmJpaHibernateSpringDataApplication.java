b10d893276a73f66ed974360e4e583f19d027926
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-03-12 13:25:06 +0000

    Initial commit

---
+++ src/main/java/ma/n7/orm_jpa_hibernatespringdata/OrmJpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package ma.n7.orm_jpa_hibernatespringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJpaHibernateSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaHibernateSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/n7/orm_jpa_hibernatespringdata/OrmJpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.n7.orm_jpa_hibernatespringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
